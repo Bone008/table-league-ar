41a7c5150357f154461e87b129cf6d10a4d5a0f0
Lukas <Bone008@users.noreply.github.com>
2019-10-31 14:00:57 +0100

    Add more test objects to investigate tracking behavior.

---
+++ Assets/Scripts/OrbitAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitAnimation : MonoBehaviour
+{
+    public float anglePerSecond = 90;
+    private float startAngle;
+
+    void Start()
+    {
+        startAngle = transform.localEulerAngles.y;
+    }
+
+    void Update()
+    {
+        float angle = (startAngle + Time.time * anglePerSecond) % 360;
+        transform.localRotation = Quaternion.Euler(0, angle, 0);
+    }
+}
