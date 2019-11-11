fb3b0ca5eb45ea8d382b6ec0d70a5f49a9e1ded0
Lukas <Bone008@users.noreply.github.com>
2019-11-11 17:11:23 +0100

    improve magnetic tower

--- Assets/Scripts/Towers/MagneticTower.cs
+++ Assets/Scripts/Towers/MagneticTower.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(CapsuleCollider))]
 public class MagneticTower : MonoBehaviour
 {
     [Tooltip("positive = attract ball, negative = repulse ball")]
-    public float pullStrength;
+    public float pullForce;
     public float maxEnergy = 0.2f;
     public float rechargeDelay = 1;
 
+    private float maxRadius;
     private float lastActivation = float.NegativeInfinity;
     private float energy = 0;
+
+    void Start()
+    {
+        maxRadius = transform.lossyScale.x * GetComponent<CapsuleCollider>().radius;
+    }
 
     void Update()
     {
@@ -38,7 +44,11 @@ public class MagneticTower : MonoBehaviour
 
         Vector3 dir = transform.position - other.transform.position;
         dir.y = 0; // Ignore vertical component.
-        Vector3 force = (pullStrength / dir.sqrMagnitude) * dir.normalized;
+        // At the range border, pull strength should be 100%,
+        // increasing linearly with proximity up to 200%.
+        // In reality, it should be quadratic, but that is way too extreme.
+        float distFactor = Mathf.Min(2, maxRadius / dir.magnitude);
+        Vector3 force = (pullForce * distFactor) * dir.normalized;
         other.attachedRigidbody.AddForce(force, ForceMode.Force);
     }
 }
