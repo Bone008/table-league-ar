452005c94cfdf0263d7d1eeb5b49d8cc373344d4
Lukas <Bone008@users.noreply.github.com>
2019-11-13 16:34:09 +0100

    Rescale barrier and fix ball preview.

--- Assets/Scripts/BallInteractionPreview.cs
+++ Assets/Scripts/BallInteractionPreview.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class BallInteractionPreview : MonoBehaviour
 {
-    public PlayerInputController player;
+    public PlayerInputController controller;
     public Renderer visual;
     
     void LateUpdate()
     {
         Vector3 pos = transform.position;
-        bool inRange = (player.transform.position - transform.position).sqrMagnitude < player.maxInteractionRange * player.maxInteractionRange;
+        bool inRange = (controller.transform.position - transform.position).sqrMagnitude < controller.maxInteractionRange * controller.maxInteractionRange;
 
         visual.enabled = inRange;
         if(inRange)
         {
-            Vector3 rot = player.transform.eulerAngles;
+            Vector3 rot = controller.transform.eulerAngles;
             rot.x = 0; // TODO allow pointing upwards
             transform.eulerAngles = rot;
         }
