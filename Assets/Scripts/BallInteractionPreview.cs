d3fe347a78207e3d5a54c27b56aecf4a09eb1c70
Lukas <Bone008@users.noreply.github.com>
2019-11-13 16:09:00 +0100

    missing

---
+++ Assets/Scripts/BallInteractionPreview.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallInteractionPreview : MonoBehaviour
+{
+    public PlayerInputController player;
+    public Renderer visual;
+    
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+        bool inRange = (player.transform.position - transform.position).sqrMagnitude < player.maxInteractionRange * player.maxInteractionRange;
+
+        visual.enabled = inRange;
+        if(inRange)
+        {
+            Vector3 rot = player.transform.eulerAngles;
+            rot.x = 0; // TODO allow pointing upwards
+            transform.eulerAngles = rot;
+        }
+    }
+}
