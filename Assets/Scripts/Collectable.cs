0bc4d4bf9c35e9cd39fc6f006e03b40bffaa9d66
Lukas <Bone008@users.noreply.github.com>
2020-01-23 18:18:09 +0100

    despawn collectables after ignoring them for a while

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -9,11 +9,31 @@ public class Collectable : NetworkBehaviour
     public CollectableType type;
     public int amount;
     public float collectDuration;
+    public float timeToLive;
     public ParticleSystem collectEffect;
+
+    private float creationTime;
+
+    [ServerCallback]
+    void Start()
+    {
+        creationTime = Time.time;
+    }
+
+    [ServerCallback]
+    void Update()
+    {
+        if(timeToLive > 0 && Time.time - creationTime > timeToLive)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     [Server]
     public void StartCollecting(Player who) {
         RpcTriggerEffect(true);
+        // Reset despawn timer whenever the player interacts with it.
+        creationTime = Time.time;
     }
 
     [Server]
