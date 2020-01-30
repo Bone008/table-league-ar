17924513fce5913dcd97f2b7e7004ef456e8feca
Lukas <Bone008@users.noreply.github.com>
2020-01-30 22:15:14 +0100

    added missing script

---
+++ Assets/Scripts/UI/SpectatorUI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SpectatorUI : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject[] toggleTargets;
+    public TrackingController trackingController;
+
+    private float lastClickTime = 0;
+    private bool uiActive = true;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // Note: eventData.clickCount does not work for touches.
+        if (eventData.clickTime - lastClickTime < 0.5f)
+            ToggleUI();
+        lastClickTime = eventData.clickTime;
+    }
+
+    private void ToggleUI()
+    {
+        uiActive = !uiActive;
+        foreach (var target in toggleTargets)
+            target.SetActive(uiActive);
+    }
+
+    public void SetFreezeCamera(bool value)
+    {
+        trackingController.enabled = value;
+    }
+}
